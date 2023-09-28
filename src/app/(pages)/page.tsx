ad2a9f9a3b715af946fe422b3587fcf923664c42
Thiago Calazans <devt.calazans@gmail.com>
2023-09-28 14:46:32 -0300

    fix: dashboard page

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,3 +1,3 @@
 export default function Page () {
-    return <div>main</div>
+    return <div>FUTURO DASHBOARD</div>
 }
--- src/client/components/header.tsx
+++ src/client/components/header.tsx
@@ -1,12 +1,12 @@
+import Link from "next/link";
 import { ThemeToggle } from "./theme-togle";
 import { Navbar } from "./ui/navbar";
-
 
 export function Header() {
   return (
     <div className="border-b flex justify-center">
       <div className="flex flex-1 h-16 items-center justify-between md:justify-start px-2  md:px-16">
-        <span>GCASPP</span>
+        <Link href={"/"}>GCASPP</Link>
         <div className="flex flex-1 items-center px-4  justify-between">
           <Navbar />
           <ThemeToggle />
