d4bbf8e550a15294f7a783783700c185bf4b6c05
Thiago Calazans <devt.calazans@gmail.com>
2023-10-03 11:41:20 -0300

    fix: api CORS next config

--- next.config.js
+++ next.config.js
@@ -4,16 +4,27 @@ const nextConfig = {
     API_BASE_URL: process.env.API_BASE_URL,
     DATABASE_URL: process.env.DATABASE_URL,
   },
-  headers: [
-    { key: "Access-Control-Allow-Credentials", value: "true" },
-    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-    {
-      key: "Access-Control-Allow-Headers",
-      value:
-        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-    },
-  ],
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
